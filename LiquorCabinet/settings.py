48c489b8b079506ee1effb82ef1aee3f284f5cc3
hallshit <nigel672@gmail.com>
2017-03-21 11:46:44 -0700

    config media_cdn

--- LiquorCabinet/settings.py
+++ LiquorCabinet/settings.py
@@ -126,10 +126,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'assets'), (PROJECT_ROOT, 'static'))
+    os.path.join(PROJECT_ROOT, 'assets'), (PROJECT_ROOT, 'media_cdn'))
 
